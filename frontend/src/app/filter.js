d1deca3d5e4c4c6bb7cf8a73aae132e6f4118052
Aditya Bose <adityabosemail@gmail.com>
2023-09-16 18:03:37 -0400

    filter

--- frontend/src/app/filter.js
+++ frontend/src/app/filter.js
@@ -2,11 +2,17 @@ export default () => {
     return (
      <div className="w-full-flex">
       <div>
-          <select className="block w-full border rounded border-gray-500 px-4 py-2 focus:outline-none text-black 500">
-          <option selected>Filter</option>
+          <select className="block w-30 border rounded border-gray-500 px-4 py-2 focus:outline-none text-black 500">
+          <option selected>Credit Hours</option>
+            <option value = "1">One</option>
+            <option value = "2">Two</option>
+            <option value = "3">Three</option>
+            <option value = "4">Four</option>
+          </select>
+      </div>
+      <div>
+        <select className="block w-3000 border rounded border-gray-500 px-4 py-2 focus:outline-none text-black 500">
             <option selected>Subject</option>
-            <option value = "Cr">Credits</option>
-            <option value="P">Prof</option>
           </select>
       </div>
      </div>
