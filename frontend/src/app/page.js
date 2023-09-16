74bc7897f6bf2558f5be2c6218e049d15430107b
Mukul Agarwal <agarmukul23@gmail.com>
2023-09-16 15:34:01 -0400

    Update course.js

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,12 +1,30 @@
-import Image from 'next/image'
 import SearchBar from './searchBar';
 import Course from './course';
+
+let defaultCourses = [
+  {
+    dept: "CS",
+    number: 18000,
+    credits: 4
+  },
+  {
+    dept: "MA",
+    number: 26100,
+    credits: 4
+  },
+]
 export default function Home() {
   return (
       <main className="min-h-screen justify-between p-24 bg-white">
+        <div className='items-center'>
           <h1 className='text-blue-500 font-bold text-5xl'>Purdue Class Finder</h1>
+        </div>
         <SearchBar/>
-        <Course dept="CS" number={18000} credits={4}/>
+        {
+          defaultCourses.map((c) => (
+            <Course key="{c}" course={c}/>
+          ))
+        }
       </main>
   )
 }
