3aac5b9faf684311bfbeb2ac59b96a41d0c3fd2b
sohampaik <soham.paik@gmail.com>
2023-09-17 02:32:42 -0400

    Formats and stores data in json file

--- backend/getdata.py
+++ backend/getdata.py
@@ -2,22 +2,12 @@ import sys
 import requests as req
 import json
 from search import *
+import re
 
 class getData():
     count = 0
     arguments = sys.argv
     url = "https://api.purdue.io/odata/Courses?$filter="
-
-    def preserve_context_search_and_replace(file_path, search_word, replace_word):
-        with open(file_path, 'r') as file:
-            file_contents = file.read()
-
-            pattern = re.compile(rf'\b{re.escape(search_word)}\b', re.IGNORECASE)
-            updated_contents = pattern.sub(lambda match: match.group().replace(search_word, replace_word),
-                                           file_contents)
-
-        with open(file_path, 'w') as file:
-            file.write(updated_contents)
 
     if 'Title' in arguments:
         title = input("Enter title: ")
@@ -64,24 +54,6 @@ class getData():
     with open('Output.json', 'w') as json_file:
         json_file.write(json.dumps(API_Data, indent=2))
 
-    j = 0
-    for i in API_Data:
-        id = API_Data['value'][j]['SubjectId']
-        filepath = "Output.json"
-        search_word = "old"
-        replace_word = "new"
-
-        # Create a dictionary with Id as the key and Abbreviation as the value
-        id_abbreviation_dict = Search.create_id_abbreviation_dict()
-
-
-
-        j += 1
+    # id_abbreviation_dict = Search.create_id_abbreviation_dict()
 
     json_file.close()
-
-# elif sys.argv[0] == 'Subjects':
-#     url = "https://api.purdue.io/odata/Subjects?$select"
-#     r = req.get(url)
-#     json_formatted_str = json.dumps(API_Data, indent=2)
-#     print(json_formatted_str)
