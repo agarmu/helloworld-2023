50f3075e31235386ff8dc8157a0540f259a36d46
Mukul Agarwal <agarmukul23@gmail.com>
2023-09-17 03:31:26 -0400

    fix script

--- backend/getdata.py
+++ backend/getdata.py
@@ -4,66 +4,93 @@ import json
 from search import *
 import re
 
-class getData():
-    count = 0
-    arguments = sys.argv
-    url = "https://api.purdue.io/odata/Courses?$filter="
+url = "https://api.purdue.io/odata/Subjects"
+r = req.get(url)
+api_data = r.json()
 
-    if 'Title' in arguments:
-        title = input("Enter title: ")
+abbr = []
+dc = {}
+for subject in api_data['value']:
+    abbr.append(subject['Abbreviation'])
+    dc[subject['Id']] = subject['Abbreviation']
 
-        count += 1
-        if count == 1:
-            url += f"contains(Title, '{title}')"
-        else:
-            url += " and "
-            url += f"contains(Title, '{title}')"
+abbr.sort()
 
-    if 'CreditHours' in arguments:
-        credits1 = input("Enter credits: ")
-
-        count += 1
-        if count == 1:
-            url += f"CreditHours eq {credits1}"
-        else:
-            url += " and "
-            url += f"CreditHours eq {credits1}"
-
-    if 'Number' in arguments:
-        courseNum = input("Enter course number: ")
-
-        count += 1
-        if count == 1:
-            url += f"Number eq '{courseNum}'"
-        else:
-            url += " and "
-            url += f"Number eq '{courseNum}'"
-
-    # print(url)
+results = []
+for abbreviation in abbr:
+    print(f"Getting info for {abbreviation}")
+    url = url = r'https://api.purdue.io/odata/Courses?$filter=Subject/Abbreviation%20eq%20%27' + abbreviation + r'%27&$orderby=Number%20asc'
     r = req.get(url)
-    # print(r.json())
-    API_Data = r.json()
-    #print(API_Data)
-    # data = json.loads(API_Data)
+    courses_data = r.json()['value']
+    for c in courses_data:
+        c['subject'] = abbreviation
+    results.extend(courses_data)
 
-    # print(API_Data['value'][0]['SubjectId'])
+for r in results:
+    r['subject'] = dc[r['SubjectId']]
 
-    # json_formatted_str = json.dumps(API_Data, indent=2)
-    # print(json_formatted_str)
+with open('out.json', 'w') as f:
+    f.write(json.dumps(results, indent=2))
+    f.close()
+# count = 0
+# arguments = sys.argv
+# url = "https://api.purdue.io/odata/Courses?$filter="
 
-    # Dumps the output of the query to Output.json
-    with open('Output.json', 'w') as json_file:
-        json_file.write(json.dumps(API_Data, indent=2))
+#     if 'Title' in arguments:
+#         title = input("Enter title: ")
 
-    id_abbreviation_dict = Search.create_id_abbreviation_dict()
+#         count += 1
+#         if count == 1:
+#             url += f"contains(Title, '{title}')"
+#         else:
+#             url += " and "
+#             url += f"contains(Title, '{title}')"
 
-    for subject in API_Data['value']:
-        if id_abbreviation_dict['Id'] == subject['value']['Id']:
-            subject['value']['Id'] = id_abbreviation_dict['Abbreviation']
+#     if 'CreditHours' in arguments:
+#         credits1 = input("Enter credits: ")
+
+#         count += 1
+#         if count == 1:
+#             url += f"CreditHours eq {credits1}"
+#         else:
+#             url += " and "
+#             url += f"CreditHours eq {credits1}"
+
+#     if 'Number' in arguments:
+#         courseNum = input("Enter course number: ")
+
+#         count += 1
+#         if count == 1:
+#             url += f"Number eq '{courseNum}'"
+#         else:
+#             url += " and "
+#             url += f"Number eq '{courseNum}'"
+
+#     # print(url)
+#     r = req.get(url)
+#     # print(r.json())
+#     API_Data = r.json()
+#     #print(API_Data)
+#     # data = json.loads(API_Data)
+
+#     # print(API_Data['value'][0]['SubjectId'])
+
+#     # json_formatted_str = json.dumps(API_Data, indent=2)
+#     # print(json_formatted_str)
+
+#     # Dumps the output of the query to Output.json
+#     with open('Output.json', 'w') as json_file:
+#         json_file.write(json.dumps(API_Data, indent=2))
+
+#     id_abbreviation_dict = Search.create_id_abbreviation_dict()
+
+#     for subject in API_Data['value']:
+#         if id_abbreviation_dict['Id'] == subject['value']['Id']:
+#             subject['value']['Id'] = id_abbreviation_dict['Abbreviation']
 
 
-    with open('Output.json', 'w') as json_file:
-        json_file.write(json.dumps(API_Data, indent=2))
+#     with open('Output.json', 'w') as json_file:
+#         json_file.write(json.dumps(API_Data, indent=2))
 
 
-    json_file.close()
+#     json_file.close()
--- backend/search.py
+++ backend/search.py
@@ -1,28 +1,3 @@
-# import sys
-# import requests as req
-# import json
-#
-# class search:
-#     url = "https://api.purdue.io/odata/Subjects"
-#     r = req.get(url)
-#     API_Data = r.json()
-#     json_formatted_str = json.dumps(API_Data, indent=2)
-#
-#     def read_json_file(cls):
-#         with open('Subjects.json', 'r') as f:
-#             data = json.loads(f.read())
-#         return data
-#
-#     l = 0
-#     data = read_json_file()  # Call the method to get the data
-#     while l < 10:
-#         print(l)
-#         subject_id = data['value'][2]['Id']
-#         subject_name = data['value'][2]['Abbreviation']
-#         print(subject_name)
-#         l += 1
-
-
 
 import requests as req
 
@@ -40,7 +15,3 @@ class Search:
 
 # Create a dictionary with Id as the key and Abbreviation as the value
 id_abbreviation_dict = Search.create_id_abbreviation_dict()
-
-# Print the dictionary
-for subject_id, abbreviation in id_abbreviation_dict.items():
-    print(f"Id: {subject_id}, Abbreviation: {abbreviation}")
