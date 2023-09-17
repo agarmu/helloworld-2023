be26eb525ee8e10998a0d12bc59218756821b6fc
Aditya Bose <adityabosemail@gmail.com>
2023-09-16 21:57:09 -0400

    Add credit filter

--- frontend/src/app/filter.js
+++ frontend/src/app/filter.js
@@ -1,12 +1,25 @@
 'use client';
 
-import Select from "react-select/base";
-
 export default Filter
 
-function Filter({subjects, currentSubject, changeSubject}) {
+function Filter({subjects, currentSubject, changeSubject, currentCredits, changeCredits}) {
     return (
      <div className="w-full-flex">
+        <div>
+            <select
+                 className="block w-3000 border rounded border-gray-500 px-4 py-2 focus:outline-none text-black 500"
+                 value={currentCredits}
+                 onChange={changeCredits}
+             >
+                <option key="creditcount-all" value={-1}>Any Credits</option>
+                 {
+                     [0,1,2,3,4,5,6,7,8,9].map((n) => (
+                         <option key={"creditcount-" + n} value={n}>{n} Credits</option>
+                     ))
+                 }
+             </select>
+
+        </div>
          <div>
              <select
                  className="block w-3000 border rounded border-gray-500 px-4 py-2 focus:outline-none text-black 500"
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -9,18 +9,32 @@ import {sortBy} from "lodash";
 export default function Home() {
     const [subjectsData, setSubjectsData] = useState([])
     const [courseData, setCourseData] = useState([])
-
+    
+    const [currentCredits, setCurrentCredits] = useState(-1)
     const [currentSubject, setCurrentSubject] = useState('AAE')
+
     let onSubjectChange = (event) => {
         let v = event.target.value
         setCurrentSubject(v)
     }
+
+    let onCreditChange = (event) => {
+      let v = event.target.value
+      setCurrentCredits(v)
+    }
+
     useEffect(() => {
         getCourses(currentSubject)
             .then((data) => {
-                setCourseData(data)
+                let filteredData;
+                if (currentCredits == -1) {
+                  filteredData = data;
+                } else {
+                  filteredData = data.filter(x => x.credits == currentCredits)
+                }
+                setCourseData(filteredData)
             })
-    }, [currentSubject]);
+    }, [currentSubject, currentCredits]);
     useEffect(() => {
         getSubjects()
             .then((data) => {
@@ -33,9 +47,12 @@ export default function Home() {
       <main className="min-h-screen justify-between p-24 bg-white">
         <div className='items-center'>
           <h1 className='text-blue-500 font-bold text-5xl'>Purdue Class Finder</h1>
-        <div>
-          <Filter subjects={subjectsData} currentSubject={currentSubject} changeSubject={onSubjectChange}/>
+          <div>
+            <Filter subjects={subjectsData} currentSubject={currentSubject} changeSubject={onSubjectChange} currentCredits={currentCredits} changeCredits={onCreditChange}/>
+          </div>
         </div>
+        <div>
+
         </div>
           <SearchBar/>
           <Courses courses={courseData} />
